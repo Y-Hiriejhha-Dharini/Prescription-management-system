0cc6752a8e099bb320c2781aaa2f52ab27982908
Dharini <hirijhaa@gmail.com>
2023-08-27 08:00:30 +0530

    Fresh Installation

---
+++ app/Http/Controllers/PrescriptionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prescription;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PrescriptionController extends Controller
+{
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+}
